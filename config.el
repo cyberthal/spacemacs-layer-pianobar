0e516224f29ff1bb021392b9b08d87e3bfbb83a5
leo3 <texas.cyberthal@gmail.com>
2020-01-28 14:22:07 +0800

    Add pianobar-command variable

--- config.el
+++ config.el
@@ -10,7 +10,12 @@
 ;;; License: GPLv3
 
 (defvar pianobar-config 'nil
-  "Set to t if your pianobar config contains your username and password.")
+  "Set to t if your pianobar config contains your username and
+  password.")
+
+(defvar pianobar-command "pianobar"
+  "If pianobar is not in your path, set this variable to
+  pianobar's path.")
 
 (defvar pianobar-station 'nil
   "Automatically connect to this station on login. Value must be
